0dd5c4b7a3d15369d559c985cfa9afcda9666abd
JohnDohnut <lgj1089@gmail.com>
2025-08-08 17:37:58 +0900

    Refactored StorageService, Implemented UserRepositoryService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,58 +16,5 @@ export class AuthService {
         private readonly jwtService : JwtService,
     ) { }
 
-    async createUser(createUserDTO: CreateUserDTO) : Promise<void> {
-
-        const user_id = createUserDTO.id;
-        const user_password = createUserDTO.password;
-
-        const hashedId = this.encryptionService.getHashedValue(user_id);
-        if(this.storageService.getFilePathIfExists(hashedId) !== null){
-            throw new ControllerException(ControllerErrorCode.USER_ALREADY_EXISTS);
-        }
-        
-        await this.storageService.create(hashedId);
-
-        const userJson: User = {
-            id: user_id,
-            password: user_password,
-            ha_mon_list: [],
-            resource_mon_list: []
-        }
-
-        const userJsonText = JSON.stringify(userJson);
-        const encrypted = this.encryptionService.encryptValue(userJsonText);
-
-        await this.storageService.write(hashedId, encrypted);
-
-        const readEnc = await this.storageService.read(hashedId);
-        const decryptedValue = this.encryptionService.decryptValue(readEnc);
-        console.log(decryptedValue);
-        
-    }
-
-    async validateUser(createUserDTO : CreateUserDTO) : Promise<void> {
-        const userId = createUserDTO.id;
-        const userPassword = createUserDTO.password;
-
-        const hashedId = this.encryptionService.getHashedValue(userId);
-
-        const userFile = await this.storageService.getFilePathIfExists(hashedId);
-
-        if(userFile === null){
-            throw new ControllerException(ControllerErrorCode.INVALID_CREDENTIALS);
-        }
-
-        const encryptedValue = await this.storageService.read(userFile);
-        const decryptedValue = this.encryptionService.decryptValue(encryptedValue);
-
-        const userJson : User = JSON.parse(decryptedValue);
-
-        if (userId !== userJson.id || userPassword !== userJson.password){
-            throw new ControllerException(ControllerErrorCode.INVALID_CREDENTIALS);
-        } 
-
-    }
-
-    async deleteUser
+  
 }
